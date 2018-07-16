888113f6abf37d6d7f168599cfbfbb04d65c330c
Patryk Gałąska <patryk.galaska@softiq.pl>
2018-07-16 15:06:47 +0200

    Test

--- src/test/java/TEST_vector2d_add.java
+++ src/test/java/TEST_vector2d_add.java
@@ -10,7 +10,9 @@ public class TEST_vector2d_add {
         Vector2D b = new Vector2D(2,7);
         a.add(b);
 
-        Assert.assertEquals( "Oczekiwane x: 7\n Otrzymano x: " + a.getX(),7,a.getX());
-        Assert.assertEquals("Oczekiwane y: 7\n Otrzymano y: " + a.getY(), 17,a.getY());
+        Assert.assertEquals( "Oczekiwane x dla a: 7\n Otrzymano x: " + a.getX(),7,a.getX());
+        Assert.assertEquals("Oczekiwane y dla a: 7\n Otrzymano y: " + a.getY(), 17,a.getY());
+        Assert.assertEquals( "Oczekiwane x dla b: 7\n Otrzymano x: " + b.getX(),2,b.getX());
+        Assert.assertEquals("Oczekiwane y dla b: 7\n Otrzymano y: " + b.getY(), 7,b.getY());
     }
 }
